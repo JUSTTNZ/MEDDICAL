d78353718671a14e826f1e0c6dd5d18036db5045
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-10-06 03:42:15 +0100

    first commit

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -5,15 +5,29 @@ import call from '../..//assets/homeassets/call.svg'
 const Info = () => {
   return (
     <>
-       <div className='bg-white p-[20px] flex  '>
+       <div className='bg-white p-[20px] flex '>
         <img src={logo} alt="logo" className='h-5 width-auto px-4'/>
-        <div>
+        <div className='flex'>
           <div>
             <img src={call} alt="call" className='h-5'/>
             <div>
               <p className='text-primary '>EMERGENCY</p>
               <p className='text-secondary'>(237)681-812-255</p>
             </div>
+          </div>
+          <div>
+            <img src={call} alt="call" className='h-5'/>
+            <div>
+              <p className='text-primary '>EMERGENCY</p>
+              <p className='text-secondary'>(237)681-812-255</p>
+            </div>
+          <div>
+            <img src={call} alt="call" className='h-5'/>
+            <div>
+              <p className='text-primary '>EMERGENCY</p>
+              <p className='text-secondary'>(237)681-812-255</p>
+            </div>
+          </div>   
           </div>
         </div>
        </div>
