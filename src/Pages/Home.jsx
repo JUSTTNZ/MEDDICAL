1c8d38f677fc310a116d86ab6fe34567b1048614
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-10-06 17:16:38 +0100

    width checking in home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
       <div className='bg-custom-homepage-image bg-cover bg-right bg-no-repeat h-screen flex flex-col justify-center p-[200px] '>
         <h3 className=' font-yeseva text-secondary text-3xl pl-24'>CARING FOR LIFE</h3>
         <h2 className='font-yeseva text-primary text-6xl align-center pl-24'>Leading the Way <br /> in Medical Excellence</h2>
-        <p className=' font-yeseva ml-24 mr-[550px] mt-8 px-2 py-2 bg-tertiary rounded-full text-primary'>Our Services</p>
+        <p className=' font-yeseva sm:w-25 lg:w-25 md:w-45  ml-24 mr-[550px] mt-8 px-2 py-2 bg-tertiary rounded-full text-primary'>Our Services</p>
       </div>
     </>
   )
