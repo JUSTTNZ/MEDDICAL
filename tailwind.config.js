650bf8f00d593a8db4ddb6c07a256500f2f10b42
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-10-07 16:49:17 +0100

    still on the homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,8 @@ export default {
         'tertiary': '#BFD2F8', //custom color for button
       },
       backgroundImage: {
-        'custom-homepage-image': "url('/src/assets/homeassets/Physician_Trends_2021_Merritt_Hawkins 1.svg')"
+        'custom-homepage-image': "url('/src/assets/homeassets/Physician_Trends_2021_Merritt_Hawkins 1.svg')",
+        'custom-appointment-image': "url('src/assets/homeassets/Rectangle 33.svg')"
       },
       fontFamily: {
         'yeseva': ['"Yeseva One"', 'cursive'], // Adding Yeseva One font
