dd3d685a179df8f79e1dbf9b28b2b06301710afc
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-10-10 05:43:23 +0100

    the background image for the contact page no de gree show

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import './App.css'
 import { BrowserRouter as Router ,Routes, Route } from 'react-router-dom'
 import Info from './Components/Info/Info'
 import Navbar from './Components/Navbar/Navbar'
-import Home from './Pages/Home'
-import { Footer } from './Pages/Footer'
+import Home  from './Pages/Home'
+import Contact  from './Pages/Contacts'
 import { About } from './Pages/About'
 import { Doctor } from './Pages/Doctor'
+import { Footer } from './Pages/Footer'
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Route path='/' element={<Home/>} />
           <Route path='/about' element={<About />}   />
           <Route path='/doctors' element={<Doctor />} />
+          <Route path='/contacts' element={<Contact/>}/>
         </Routes>
       
         
