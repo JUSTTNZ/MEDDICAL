bd789819ea306fd3fea4cbb5420e3006515544aa
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-11-20 20:53:50 +0100

    doctors done

--- src/Doctors/dochero.jsx
+++ src/Doctors/dochero.jsx
@@ -1,18 +1,18 @@
 
 export const DocHero= () => {
     return(
-        <div className="md:h-[250px] h-[150px] w-full flex flex-col abthero-bg md:p-12 sm:p-4 xs:p-2">
+        <div className="lg:h-[400px] h-[150px] w-full flex flex-col abthero-bg md:p-12 sm:p-4 xs:p-2">
   <div className="md:p-12 md:pl-[150px] pl-[100px] mt-3 sm:pl-4 xs:pl-2">
     <nav className="text-sm breadcrumbs">
       <ul className="flex">
         <li className="mr-1 text-[#1F2B6C]">
-          <a href="#" className=" ">Home</a>
+          <a href="#" className=" text-lg  font-yeseva">Home</a>
         </li>
         <li className="mr-1 text-[#1F2B6C]">/</li>
-        <li className="text-[#1F2B6C]">Doctors</li>
+        <li className="text-[#1F2B6C] text-lg  font-yeseva">Doctors</li>
       </ul>
     </nav>
-    <h2 className="text-2xl font-bold mt-4 text-[#1F2B6C] sm:text-xl xs:text-lg">Our Doctors</h2>
+    <h2 className="font-bold mt-4 text-[#1F2B6C] text-4xl">Our Doctors</h2>
   </div>
 </div>
     )
